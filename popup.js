d436d2dfefdacd2aa6d723bb61eea485ac5af645
genius0412 <genius0412@naver.com>
2020-08-10 17:14:11 +0900

    c

---
+++ popup.js
@@ -0,0 +1,8 @@
+ let bannedUrl = new Array();
+
+ document.addEventListener('DOMContentLoaded', function() {
+    $('#go').addEventListener('click', function() {
+        if($('#server > div.menu > div.item.active.selected').attr('data-value')){
+        }
+    });
+});
