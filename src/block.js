eebc31cb3f9150dde1350cb1c960ff8d62b4b5e6
genius0412 <genius0412@naver.com>
2020-08-14 00:08:02 +0900

    commit-really finished!!!!

--- src/block.js
+++ src/block.js
@@ -1,5 +1,5 @@
 const BanListTag = 'URLBAN-BanList';
 
 localStorage.getItem(BanListTag).forEach(e => {
-    if(location.href.includes(e)) window.open('')
+    if(location.href.includes(e)) window.open('https://student-developers-korea.github.io/URL-BAN/src/blocked/index.html');
 })
